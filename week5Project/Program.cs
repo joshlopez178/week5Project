fb7df771ebce1c11be1b826d0b42fda8ab842fd2
Josh <j0522469@amarillocollege.com>
2021-01-11 16:13:53 -0600

    Add project files.

---
+++ week5Project/Program.cs
@@ -0,0 +1,48 @@
+﻿//Joshua Lopez 
+//COSC 1320 001
+//9-26-2019
+//Week 5 Exam 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace week5Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            //I used this to get the squared values of the numbers
+            
+            Console.WriteLine("Here are the number values squared..");
+
+            for (int i = 0; i < 101; i++)
+            {
+               
+
+                Console.WriteLine("{0} {1}",
+                    i.ToString().PadLeft(5, ' '), Math.Pow(i, 2));
+
+                //This creates a .txt file with all the values printed on it
+
+                TextWriter num = new StreamWriter("squaredNumbers.txt", true);
+                num.WriteLine(i*i);
+ 
+                num.Close();
+            }
+
+           
+           
+            Console.WriteLine("File Created..");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ week5Project/numbersSquared.cs
@@ -0,0 +1,43 @@
+﻿//Joshua Lopez
+//CSOC 1320
+//9-26-2019
+//Week 5 project
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace week5Project
+{
+    class numbersSquared
+    {
+
+        public static void squares()
+        {
+
+           //
+
+            try
+            {
+                TextWriter num = new StreamWriter("squaredNumbers.txt", true);
+                num.WriteLine("here is the list", num );
+                num.Close();
+                Console.WriteLine("File Created..");
+
+            }
+            catch(Exception i)
+            {
+                Console.Write(i);
+
+            }
+
+            Console.Read();
+
+        }
+        
+
+        
+    }
+}
